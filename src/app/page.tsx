eece403b95da3dece0b85588edd7d38f74fd1200
gflexx <glennenri24@gmail.com>
2025-08-24 17:41:16 +0300

    Added shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  function btnClicked() {
+    console.log("Clicked");
+  }
   return (
     <>
-      <p className="text-center">PPP</p>
+      <p className="text-c2xlnter">Dashboard</p>
+      <Button onClick={() => btnClicked()}>CLick Me</Button>
     </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='mb-3'>Navbar</div>
+  )
+}
+
+export default Navbar
