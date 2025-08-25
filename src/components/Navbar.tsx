42c78c54b81c8378c729de1b066901093fa4001e
gflexx <glennenri24@gmail.com>
2025-08-25 13:21:38 +0300

    Added avatar and dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
+import logo from '../../assets/Group 1686551587.svg'
+import profileImg from '../../assets/1681073508132.jpg'
+import Image from 'next/image'
+import { Avatar, AvatarImage } from './ui/avatar'
 
 function Navbar() {
   return (
-    <div className='mb-3'>Navbar</div>
+    <div className='bg-primary dark:bg-slate-700 text-white py-3 px-5 flex justify-between'>
+      <Link href='/'>
+        <Image src={logo} alt='logo' width={15} className='rounded-full'/>
+      </Link>
+      <Avatar>
+        <AvatarImage src={profileImg.src}/>
+      </Avatar>
+    </div>
   )
 }
 
