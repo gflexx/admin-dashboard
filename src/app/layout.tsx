4ac4e07d4c511f2aaed65ef39698f019844a46be
gflexx <glennenri24@gmail.com>
2025-08-27 20:10:50 +0300

    Added theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,18 +27,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] md:w-1/6">
-            <Sidebar />
+        <ThemeProvider
+          attribute="class"
+          enableSystem={false}
+          defaultTheme="light"
+          storageKey="dashboard-theme"
+        >
+          <Navbar />
+          <div className="flex">
+            <div className="hidden md:block h-[100vh] md:w-1/6">
+              <Sidebar />
+            </div>
+            <div className="w-full p-5">{children}</div>
+            <Toaster />
           </div>
-          <div className="w-full p-5">{children}</div>
-          <Toaster />
-        </div>
+        </ThemeProvider>
       </body>
     </html>
   );
